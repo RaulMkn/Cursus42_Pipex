a69c0f7caecba34d71061fbc24a3882d2108e7a9
rmakende <rmakendenguerodriguez@gmail.com>
2024-12-15 00:41:13 +0100

    update

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: rmakende <rmakende@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 16:56:47 by rmakende          #+#    #+#             */
-/*   Updated: 2024/12/10 22:30:36 by rmakende         ###   ########.fr       */
+/*   Updated: 2024/12/15 00:35:56 by rmakende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,6 @@
 # include <string.h>
 # include <errno.h>
 # include <sys/wait.h>
-
-typedef struct t_int
-{
-	int		flag;
-	pid_t	pid;
-}	t_returner;
 
 void	execute_command(char *cmd, char **envp);
 
