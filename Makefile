89d6ebedd7be6d2d8c33e865050f3f14227be608
rmakende <rmakende@MakenPC>
2025-05-14 22:04:59 +0200

    update

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: rmakende <rmakende@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/10/24 21:07:40 by rmakende          #+#    #+#              #
-#    Updated: 2025/01/16 20:12:28 by rmakende         ###   ########.fr        #
+#    Updated: 2025/05/14 22:03:14 by rmakende         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,8 +20,13 @@ LIBRARY = $(LIBFT_DIR)/libft.a
 SRCS = pipex.c\
 	   pipex_utils.c
 
+BONUS = pipex_bonus.c\
+	   pipex_utils_bonus.c
+
 OBJS = $(SRCS:.c=.o)
+BNS = $(BONUS:.c=.o)
 NAME = pipex
+NAME_BONUS = pipex_bonus
 
 all: $(LIBRARY) $(PRINTF) $(NAME)
 
@@ -31,12 +36,17 @@ $(LIBRARY):
 $(NAME): $(OBJS)
 	$(CC) $(CFLAGS) -o $(NAME) $(OBJS) $(LIBRARY)
 
+bonus: $(LIBRARY) $(PRINTF) $(NAME_BONUS)
+$(NAME_BONUS): $(BNS)
+	$(CC) $(CFLAGS) -o $(NAME_BONUS) $(BNS) $(LIBRARY)
+
 clean:
 	rm -f $(OBJS)
 	cd $(LIBFT_DIR) && $(MAKE) fclean
 
 fclean: clean
 	rm -f $(NAME)
+	rm -rf $(BNS)
 	cd $(LIBFT_DIR) && $(MAKE) fclean
 
 re: fclean all
--- pipex_bonus.c
+++ pipex_bonus.c
@@ -6,7 +6,54 @@
 /*   By: rmakende <rmakende@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 21:26:56 by rmakende          #+#    #+#             */
-/*   Updated: 2025/05/14 21:26:57 by rmakende         ###   ########.fr       */
+/*   Updated: 2025/05/14 22:02:16 by rmakende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pipex.h"
+
+void	if_forker(int fd[2], char *argv[], char **envp, int *i)
+{
+	dup2(fd[1], STDOUT_FILENO);
+	close(fd[0]);
+	execute_command(argv[*i], envp);
+}
+
+void	else_forker(int fd[2], pid_t pid)
+{
+	int	status;
+
+	dup2(fd[0], STDIN_FILENO);
+	close(fd[1]);
+	waitpid(pid, &status, 0);
+}
+int	main(int argc, char **argv, char **envp)
+{
+	int		fd[2];
+	pid_t	pid;
+	int		in_fd;
+	int		out_fd;
+    int     i;
+
+	if (argc < 5)
+		exit(1);
+	in_fd = open(argv[1], O_RDONLY);
+	out_fd = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	dup2(in_fd, STDIN_FILENO);
+	close(in_fd);
+    i = 2;
+	while (i < argc - 2)
+	{
+		pipe(fd);
+		pid = fork();
+		if (pid == 0)
+			if_forker(fd, argv, envp, &i);
+		else
+			else_forker(fd, pid);
+        i++;
+	}
+	dup2(out_fd, STDOUT_FILENO);
+	close(out_fd);
+	execute_command(argv[argc - 2], envp);
+	return (0);
+}
